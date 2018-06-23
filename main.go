d8a149bdb17695ade488bf35fd9dd9c2b4765e74
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-06-23 16:56:26 +0300

    add basic support for static files

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 
 	http.HandleFunc("/", indexHandler)
+	//http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
+	http.Handle("/html/static/", http.StripPrefix("/html/static/", http.FileServer(http.Dir("html/static/"))))
 
 	http.ListenAndServe(":8080", nil)
 
