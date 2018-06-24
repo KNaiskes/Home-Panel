76d85a7fb0a0777c491691c74084c49b771f466f
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-06-24 23:05:14 +0300

    add basic login page template

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"net/http"
 	"html/template"
-	"path"
 	"log"
+	"net/http"
+	"path"
 )
 
 type BasicElements struct {
 	Title string
 }
+
+func main() {
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.Handle("/html/static/", http.StripPrefix("/html/static/",
+		http.FileServer(http.Dir("html/static/"))))
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	page := BasicElements{"Index"}
@@ -25,12 +37,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	page := BasicElements{"Login-Page"}
+	fp := path.Join("html/templates/", "login.html")
 
-	http.HandleFunc("/", indexHandler)
-	//http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
-	http.Handle("/html/static/", http.StripPrefix("/html/static/", http.FileServer(http.Dir("html/static/"))))
-
-	http.ListenAndServe(":8080", nil)
-
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tmpl.Execute(w, page)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
