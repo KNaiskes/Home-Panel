e3c636282794107e849cad5be0545ec7494ffb7c
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-06-26 23:46:58 +0300

    removed unnecessary code

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 	"path"
 )
 
-type BasicElements struct {
-	Title string
-}
-
 type TemperatureHum struct {
 	Title string
 	DateTime []string
@@ -31,28 +27,26 @@ func main() {
 
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	page := BasicElements{"Index"}
 	fp := path.Join("html/templates/", "index.html")
 
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = tmpl.Execute(w, page)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	page := BasicElements{"Login-Page"}
 	fp := path.Join("html/templates/", "login.html")
 
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = tmpl.Execute(w, page)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
