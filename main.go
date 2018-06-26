2a3771a80e029fa1cd5671d4248d5e876be69286
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-06-26 23:15:53 +0300

    add temperature and humidity example table

--- main.go
+++ main.go
@@ -11,10 +11,17 @@ type BasicElements struct {
 	Title string
 }
 
+type TemperatureHum struct {
+	Title string
+	DateTime []string
+	Temperature []string
+}
+
 func main() {
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/tempHum", temphumiHandler)
 	http.Handle("/html/static/", http.StripPrefix("/html/static/",
 		http.FileServer(http.Dir("html/static/"))))
 
@@ -40,6 +47,22 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	page := BasicElements{"Login-Page"}
 	fp := path.Join("html/templates/", "login.html")
+
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tmpl.Execute(w, page)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func temphumiHandler(w http.ResponseWriter, r *http.Request) {
+	DateTime := []string {"20","29", "40"} //just for testing
+	Temperature := []string {"18-06-26", "18-06-26", "18-06-26"} //just for testing
+	page := TemperatureHum{"Temperature/Humidity", DateTime, Temperature}
+	fp := path.Join("html/templates/", "temphum.html")
 
 	tmpl, err := template.ParseFiles(fp)
 	if err != nil {
