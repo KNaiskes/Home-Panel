d026042d219f79aff94a5e9b74d3ddca851a380b
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-07-25 20:35:58 +0300

    some variable names changes

--- src/app/mqtt/mqtt.go
+++ src/app/mqtt/mqtt.go
@@ -5,7 +5,7 @@ import (
 )
 
 func ChangeState(command string, topic string) {
-
+	// function for every two state (on/off) electrical devices
 	switch command {
 		case "on":
 			cmd := exec.Command("mosquitto_pub", "-m", "on", "-t", topic)
@@ -17,7 +17,7 @@ func ChangeState(command string, topic string) {
 }
 
 func ChangeColor(color string, topic string) {
-
+	// function for led strips and bubles rgb bulbs
 	cmd := exec.Command("mosquitto_pub", "-m", color, "-t", topic)
 	cmd.Output()
 }
