a926c98e85cb91e3d2ba19e22c58cc5591cf4596
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-08-12 10:28:01 +0300

    added firewall script to open ports

---
+++ firewall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+if [ "$EUID" -ne 0 ] 
+then
+	echo "You must me root, to run this script"
+	exit
+fi
+
+sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+
+sudo ip6tables -I INPUT -p tcp --dport 8080 -j ACCEPT
+sudo ip6tables -I INPUT -p tcp --dport 1883 -j ACCEPT
