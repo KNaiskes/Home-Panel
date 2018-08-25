5bdffda7e60f27a43133e44d00b38b4515843322
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-08-25 21:41:14 +0300

    rewrote mqtt change color function

--- src/app/mqtt/mqtt.go
+++ src/app/mqtt/mqtt.go
@@ -1,9 +1,7 @@
 package mqtt
 
-
 import (
 	MQTT "github.com/eclipse/paho.mqtt.golang"
-	//"os/exec"
 )
 
 func ChangeState(command string, topic string) {
@@ -18,9 +16,6 @@ func ChangeState(command string, topic string) {
 		panic(token.Error())
 	}
 
-	//token := c.Publish("ledStrip", 0, false, "on")
-	//token.Wait()
-	//c.Disconnect(250)
 	switch command {
 	case "on":
 		token := c.Publish(topic, 0, false, "on")
@@ -33,25 +28,19 @@ func ChangeState(command string, topic string) {
 	}
 }
 
+func ChangeColor(color string, topic string) {
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("tcp://192.168.1.30:1883")
+	opts.SetClientID("mqttServer")
+	opts.SetCleanSession(true)
 
-//import (
-//	"os/exec"
-//)
-//
-//func ChangeState(command string, topic string) {
-//	// function for every two state (on/off) electrical devices
-//	switch command {
-//		case "on":
-//			cmd := exec.Command("mosquitto_pub", "-m", "on", "-t", topic)
-//			cmd.Output()
-//		case "off":
-//			cmd := exec.Command("mosquitto_pub", "-m", "off", "-t", topic)
-//			cmd.Output()
-//	}
-//}
-//
-//func ChangeColor(color string, topic string) {
-//	// function for led strips and rgb bulbs
-//	cmd := exec.Command("mosquitto_pub", "-m", color, "-t", topic)
-//	cmd.Output()
-//}
+	c := MQTT.NewClient(opts)
+
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	token := c.Publish(topic, 0, false, color)
+	token.Wait()
+	c.Disconnect(250)
+}
