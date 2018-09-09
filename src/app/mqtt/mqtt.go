1e6a48dfaf51d1ecf9c720bea865e4ebb6cf3e32
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-09-09 20:33:09 +0300

    added basic dht22 function

--- src/app/mqtt/mqtt.go
+++ src/app/mqtt/mqtt.go
@@ -2,6 +2,10 @@ package mqtt
 
 import (
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 const myServer = "tcp://192.168.1.30:1883"
@@ -46,4 +50,35 @@ func ChangeColor(color string, topic string) {
 	token := c.Publish(topic, 0, false, color)
 	token.Wait()
 	c.Disconnect(250)
+}
+
+func onMessageReceived(client MQTT.Client, message MQTT.Message) {
+	if string(message.Payload()) == "temperature" {
+		fmt.Println("Got temperature")
+	}
+}
+
+func Dht22Mqtt() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	topic := "SensorDht22"
+	qos := 0
+	clientid := "clientid"
+
+	connOpts := MQTT.NewClientOptions().AddBroker(myServer).SetClientID(clientid).SetCleanSession(true)
+
+	connOpts.OnConnect = func(c MQTT.Client) {
+		if token := c.Subscribe(topic, byte(qos), onMessageReceived); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+
+	client := MQTT.NewClient(connOpts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	<-c
 }
